abe5c21616eb8081fc97197bbdc46c0d6d96f1d2
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-24 21:52:42 +0800

    Add files via upload

---
+++ data/point_2.js
@@ -0,0 +1,11 @@
+var json_point_2 = {
+"type": "FeatureCollection",
+"name": "point_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Pintu Masuk Pasar Baroe", "desc": "Jl. Ps. Baru, RT.1\/RW.3, Ps. Baru, Kecamatan Sawah Besar, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10710, Indonesia", "X": "106.832525", "Y": "-6.16129" }, "geometry": { "type": "Point", "coordinates": [ 106.8325257, -6.16129 ] } },
+{ "type": "Feature", "properties": { "name": "Bakmi Gang Kelinci - Pasar baru", "desc": "Bakmi Gang Kelinci - Pasar baru, Jl. Kelinci Raya No.1-3, Ps. Baru, Kecamatan Sawah Besar, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10710, Indonesia", "X": "106.833720", "Y": "-6.1627447" }, "geometry": { "type": "Point", "coordinates": [ 106.8337206, -6.1627447 ] } },
+{ "type": "Feature", "properties": { "name": "Metro Atom Plaza", "desc": "Metro Atom Plaza, 6, Jl. Samanhudi No.1, RT.1\/RW.3, Ps. Baru, Kecamatan Sawah Besar, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10710, Indonesia", "X": "106.832268", "Y": "-6.1608795" }, "geometry": { "type": "Point", "coordinates": [ 106.8322684, -6.1608795 ] } },
+{ "type": "Feature", "properties": { "name": "Pos Bloc Jakarta", "desc": "Pos Bloc Jakarta, Jl. Pos No.2, Pasar Baru, Sawah Besar, Central Jakarta City, Jakarta 10710, Indonesia", "X": "106.833654", "Y": "-6.1668678" }, "geometry": { "type": "Point", "coordinates": [ 106.8336548, -6.1668678 ] } }
+]
+}
